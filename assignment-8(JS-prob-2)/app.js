218f40d9e660de0aee70a45557dee68b0f1fe914
Shimul59 <skanti59@gmail.com>
2021-03-07 11:45:59 +0600

    Best friend find JS prob

--- assignment-8(JS-prob-2)/app.js
+++ assignment-8(JS-prob-2)/app.js
@@ -9,10 +9,10 @@ var name1 = prompt('Enter your first friend Name ?');
     longName = Math.max(name1Len, name2Len, name3Len);
 
 if (longName == name1Len){
-    document.write('<h4> Your best friend name is </h4> ' +'<h2>'+ name1 +'</h2>')
+    document.write('<h4> Your best friend name is  : </h4>'+ '<h2>' + name1 +'</h2>')
 }
 else if(longName == name2Len){
-    document.write('<h4> Your best friend name is </h4> ' + '<h2>'+ name2 +'</h2>');
+    document.write('<h4> Your best friend name is  : </h4>'+ '<h2>' + name2 +'</h2>');
 }else{
-    document.write('<h4> Your best friend name is </h4> ' + '<h2>'+ name3 +'</h2>')
+    document.write('<h4> Your best friend name is  : </h4>'+ '<h2>' + name3 +'</h2>')
 };
