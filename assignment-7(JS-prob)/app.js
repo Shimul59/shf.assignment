7f2745c9d224be3ddc00e8c20c1b06534b1f308a
Shimul59 <skanti59@gmail.com>
2021-03-07 11:29:16 +0600

    Hotel cost cal js prob solve

--- assignment-7(JS-prob)/app.js
+++ assignment-7(JS-prob)/app.js
@@ -7,12 +7,12 @@ var afterTwenty = 100
 
 if(day <= 10 ){
     var totalCost = day * firstTen;
-    document.write('<h2>For ' + day +' days You have to pay :- ' + totalCost +' $ </h2>');
+    document.write('<h2>For ' + day +' days You have to pay :  ' + totalCost +' $ </h2>');
 }
 else if(day <= 20){
     var totalCost = (firstTen * 10) + ((day-10) * firstTwenty);
-    document.write('<h2>For </h2>' + day +'<h2> days You have to pay : </h2>' + totalCost +' $');
+    document.write('<h2>For ' + day +' days You have to pay :  ' + totalCost +' $ </h2>');
 }else{
     var totalCost = (firstTen * 10) + (firstTwenty * 10) + ((day-20) * afterTwenty);
-    document.write('<h2>For </h2>' + day +'<h2> days You have to pay : </h2>' + totalCost +' $');
+    document.write('<h2>For ' + day +' days You have to pay :  ' + totalCost +' $ </h2>');
 }
